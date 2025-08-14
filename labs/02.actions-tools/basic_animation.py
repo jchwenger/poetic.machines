61ead4077d428b1bd4acdf6b1c96ae0fa81084b4
jchwenger <34098722+jchwenger@users.noreply.github.com>
2025-08-14 16:43:38 +0100

    02.action-tools |

---
+++ labs/02.actions-tools/basic_animation.py
@@ -0,0 +1,22 @@
+from py5canvas import *
+
+
+def setup():
+    create_canvas(512, 512)
+    frame_rate(60)
+
+
+def draw():
+    background(0, 0, 0, 8)  # Clear with alpha will create the "trail effect"
+    push()
+    # Center of screen
+    translate(width / 2, height / 2)
+    # Draw rotating circle
+    fill(255, 0, 0)
+    stroke(255)
+    rotate(frame_count * 0.05)
+    circle(100, 0, 20)
+    pop()
+
+
+run()
---
+++ labs/02.actions-tools/fonts.py
@@ -0,0 +1,39 @@
+from py5canvas import *
+
+
+def setup():
+    create_canvas(512, 512)
+    background(255)
+    frame_rate(60)
+
+
+def draw():
+    background(255)
+    no_stroke()
+
+    fill(0)
+    text_font("Times New Roman")
+    # text_style("normal")
+    text_size(35)
+    text("hello", [30, 50])
+
+    fill(255, 0, 0)
+    text_font("Courier New")
+    text_style("italic")
+    text_size(65)
+    text("world!", [80, 100])
+
+    fill(0, 255, 0)
+    text_font("Helvetica")
+    text_style("bold")
+    text_size(65)
+    text("goodbye", [140, 300])
+
+    fill(0, 0, 255)
+    text_font("Verdana")
+    text_style("bolditalic")
+    text_size(75)
+    text("universe!", [60, 400])
+
+
+run()
