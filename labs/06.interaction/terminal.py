a46910aa45d647511fa97e1a0e708f3046370322
jchwenger <34098722+jchwenger@users.noreply.github.com>
2025-09-08 17:44:00 +0200

    labs/06.interaction/terminal | handling special keys

--- labs/06.interaction/terminal.py
+++ labs/06.interaction/terminal.py
@@ -56,18 +56,22 @@ def key_pressed(key):
 
     # many control keys we don't want (KP: keypad)
     # TODO: implement DELETE (remove forward), arrow keys to move cursor...
+    # (the following is an instruction for the formatter, ruff)
+    # fmt: off
     if key in [
-        "ESCAPE", "TAB", "INSERT", "DELETE", "RIGHT", "LEFT", "DOWN", "UP",
-        "PAGE_UP", "PAGE_DOWN", "HOME", "END", "CAPS_LOCK", "SCROLL_LOCK",
-        "NUM_LOCK", "PRINT_SCREEN", "PAUSE", "F1", "F2", "F3", "F4", "F5",
-        "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15",
-        "F16", "F17", "F18", "F19", "F20", "F21", "F22", "F23", "F24", "F25",
+        "ESCAPE", "TAB", "INSERT", "DELETE",
+        "RIGHT", "LEFT", "DOWN", "UP", "PAGE_UP", "PAGE_DOWN", "HOME", "END",
+        "CAPS_LOCK", "SCROLL_LOCK", "NUM_LOCK", "PRINT_SCREEN", "PAUSE",
+        "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11",
+        "F12", "F13", "F14", "F15", "F16", "F17", "F18", "F19", "F20", "F21",
+        "F22", "F23", "F24", "F25",
         "KP_0", "KP_1", "KP_2", "KP_3", "KP_4", "KP_5", "KP_6", "KP_7", "KP_8",
         "KP_9", "KP_DECIMAL", "KP_DIVIDE", "KP_MULTIPLY", "KP_SUBTRACT",
-        "KP_ADD", "KP_ENTER", "KP_EQUAL", "LEFT_SHIFT", "LEFT_CONTROL",
-        "LEFT_ALT", "LEFT_SUPER", "RIGHT_SHIFT", "RIGHT_CONTROL", "RIGHT_ALT",
-        "RIGHT_SUPER",
+        "KP_ADD", "KP_ENTER", "KP_EQUAL",
+        "LEFT_SHIFT", "LEFT_CONTROL", "LEFT_ALT", "LEFT_SUPER", "RIGHT_SHIFT",
+        "RIGHT_CONTROL", "RIGHT_ALT", "RIGHT_SUPER",
     ]: return
+    # fmt: on
 
     if key == "BACKSPACE":
         # if the last line isn't emtpy
