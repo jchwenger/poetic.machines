5ace869ec159ff0d3c4b7cf690d27095d7603e6b
jchwenger <34098722+jchwenger@users.noreply.github.com>
2025-09-10 16:03:41 +0200

    labs/02.actions-tools/silencio |

--- labs/02.actions-tools/silencio.py
+++ labs/02.actions-tools/silencio.py
@@ -16,10 +16,14 @@ def draw():
     text_font("Courier New")
     text_size(25)
 
-    text("silencio silencio silencio", [60, 50])
-    text("silencio silencio silencio", [60, 100])
-    text("silencio          silencio", [60, 150])
-    text("silencio silencio silencio", [60, 200])
-    text("silencio silencio silencio", [60, 250])
+    # text takes in:
+    #   - the string to display
+    #   - the x coordinate (horizontal)
+    #   - the y coordinate (vertical – from the top down!)
+    text("silencio silencio silencio", 60, 50)
+    text("silencio silencio silencio", 60, 100)
+    text("silencio          silencio", 60, 150)
+    text("silencio silencio silencio", 60, 200)
+    text("silencio silencio silencio", 60, 250)
 
 run()
