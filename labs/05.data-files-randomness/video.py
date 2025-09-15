a9192317efb0f6609eb5a3876ee2c7145577cc61
jchwenger <34098722+jchwenger@users.noreply.github.com>
2025-09-15 22:14:24 +0100

    labs/05.data-files-randomness/[video, data/fluxus-instructions.low] |

---
+++ labs/05.data-files-randomness/video.py
@@ -0,0 +1,27 @@
+from py5canvas import *
+
+video = VideoInput(
+    # if you do not give a file name, your webcam is used instead
+    "data/fluxus-instructions.low.mp4",
+    # the next arguments allow you to filp the video horizontally or vertically
+    # flipped=True,
+    # vertical_flipped=True
+)
+
+def setup():
+    # print(f"video size {video.size}")
+    create_canvas(*video.size)
+
+def draw():
+    background(0)
+
+    # extract the next frame
+    frame = video.read()
+
+    # place the frame on the canvas
+    image(frame, 0, 0)
+
+    # the last two arguments allow you to resize the video
+    # image(frame, 0, 0, width/2, height/2)
+
+run()
