407458b4f0ba7d9df1df8568ef2ef12d57d05e25
jchwenger <34098722+jchwenger@users.noreply.github.com>
2025-09-15 22:39:56 +0100

    labs/02.actions-tools/basic_animation |

--- labs/02.actions-tools/basic_animation.py
+++ labs/02.actions-tools/basic_animation.py
@@ -4,11 +4,13 @@ from py5canvas import *
 def setup():
     create_canvas(512, 512)
     frame_rate(60)
+    background(0, 0, 0)
 
 
 def draw():
     # Clear with alpha will create the "trail effect"
     background(0, 0, 0, 8)
+
     push()
     # Center of screen
     translate(width / 2, height / 2)
@@ -18,6 +20,16 @@ def draw():
     rotate(frame_count * 0.05)
     circle(100, 0, 20)
     pop()
+
+    # push()
+    ## Move somewhere to the bottom right
+    # translate(width - 80, height - 80)
+    ## No fill, draw in blue
+    # no_fill()
+    # stroke(0, 0, 255)
+    # rotate(-frame_count * 0.1)
+    # rectangle(0, 0, 40, 40)
+    # pop()
 
 
 run()
