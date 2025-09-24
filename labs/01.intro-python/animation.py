9d991ef2ee76c830fc3223f3ec777f8d6ea5d09c
jchwenger <34098722+jchwenger@users.noreply.github.com>
2025-09-24 21:45:09 +0100

    labs/01.intro-python/animation |

--- labs/01.intro-python/animation.py
+++ labs/01.intro-python/animation.py
@@ -11,14 +11,26 @@ def draw():
     # Clear with alpha will create the "trail effect"
     background(0, 0, 0, 8)
 
-    push()
     # Center of screen
     translate(width / 2, height / 2)
-    # Draw rotating circle
+    
+    # Rotating the canvas
+    # (try changing the number, or adding `-` before)
+    rotate(frame_count * 0.05)
+
+    # Draw a circle
     fill(255, 0, 0)
     stroke(255)
-    rotate(frame_count * 0.05)
-    circle(100, 0, 20)
-    pop()
+    circle(0, 100, 80)
+
+    # Something to try: 
+    # uncomment the next lines
+    # (select them & cmd or ctrl + / )
+    # text_align(CENTER)
+    # text_size(200)
+    # no_stroke()
+    # fill(255)
+    # text("?", 0, 0)
 
 run()
+
