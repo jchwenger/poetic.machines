78e3b300156dec5849eaf1e20fc673536ab1b02a
jchwenger <34098722+jchwenger@users.noreply.github.com>
2025-09-24 22:06:58 +0100

    labs/02.actions-tools/fonts | deet

--- labs/02.actions-tools/fonts.py
+++ labs/02.actions-tools/fonts.py
@@ -15,25 +15,25 @@ def draw():
     text_font("Times New Roman")
     # text_style("normal")
     text_size(35)
-    text("hello", [30, 50])
+    text("hello", 30, 50)
 
     fill(255, 0, 0)
     text_font("Courier New")
     text_style("italic")
     text_size(65)
-    text("world!", [80, 100])
+    text("world!", 80, 100)
 
     fill(0, 255, 0)
     text_font("Helvetica")
     text_style("bold")
     text_size(65)
-    text("goodbye", [140, 300])
+    text("goodbye", 140, 300)
 
     fill(0, 0, 255)
     text_font("Brush Script MT")
     text_style("bolditalic")
     text_size(75)
-    text("universe!", [60, 400])
+    text("universe!", 60, 400)
 
 
 run()
