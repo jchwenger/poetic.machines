86f59e10cc96f938c96d2b80defdab1993d2b3c1
jchwenger <34098722+jchwenger@users.noreply.github.com>
2025-09-27 17:24:56 +0100

    labs/05.data-files-randomness/silencio_random_erasure |

--- labs/05.data-files-randomness/silencio_random_erasure.py
+++ labs/05.data-files-randomness/silencio_random_erasure.py
@@ -17,8 +17,6 @@ def setup():
 
 
 def draw():
-    global n_line
-    global n_col
     global random_line
     global random_col
 
@@ -34,10 +32,11 @@ def draw():
 
     sleep_time = 0.2
 
-    # loop 1
+    # here we have the same logic as last week,
+    # with only one big difference: our conditional
+    # is now 
     for i in range(n_line):
         line = ""
-        # loop 2
         for j in range(n_col):
             if i == random_line and j == random_col:
                 line += " " * (len(word) + 1)
@@ -46,8 +45,40 @@ def draw():
         text(line, [60, 50 + 50 * i])
 
     if sketch.frame_count % step_every == 0:
-        random_line = np.random.randint(0, n_line - 1)
-        random_col = np.random.randint(0, n_col - 1)
+        # create a random number in [0,1], multiply it by the number we want,
+        # then turn that to an int (this works because `random` never actually
+        # outputs 1, so we never ever reach `n_line` or `n_col`, always a bit
+        # below, and the decimal part gets chopped off by int() (it's a *floor*
+        # rather than a *ceiling* operation)
+        random_line = int(random() * n_line)
+        random_col = int(random() * n_col)
+
+        # for sanity you can always check
+        # print(random_line, random_col)
 
 
 run()
+
+# IDEAS, to make it your own:
+# - You might start feeling slightly sick of this silentious example, and this,
+#   I assure you, is a sign of sanity! Rest assured, however: just like with
+#   the stages of grief, acceptance is coming soon, I promise (after reading
+#   week, to be precise, when we see this **for the last time** :}). For this
+#   one, the important thing is that you understand what the random generation
+#   does, and what it gives you. Once you understand that you can get random
+#   numbers between [0,1] (never reaching one), and that you can scale those
+#   numbers with multiplication, and potentially turn those floats into ints,
+#   then you have the tools you need to apply randomness to any parameter you
+#   like! Fun thing to try:
+#   - Change the word (lol, but yes, do that).
+#   - If you implemented a version where you draw each word separately, then
+#     instead of replacing that word with blanks, you could do a translate with
+#     a random destination, or a rotate with a random angle!
+#   - Remember how we #BrokeTheSilence using colours and such? Now you can use
+#     random numbers to select the integer in your RGBA values, or the size of
+#     the text, or even the font, if you define a list of font names at the
+#     start of the sketch!
+#   - Also, you could inject randomness in `step_every`, by picking a new
+#     random `step_every` in the same spot where we currently select
+#     `random_line` and `random_col`, creating a very different kind of
+#     movement in the piece!
