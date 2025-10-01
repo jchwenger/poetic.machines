e2972c251087187b16d1ab92c0202318038a1180
jchwenger <34098722+jchwenger@users.noreply.github.com>
2025-10-01 16:32:41 +0100

    labs/01.intro-python/hello_world |

---
+++ labs/01.intro-python/hello_world.py
@@ -0,0 +1,9 @@
+# Two notes:
+# - This is a common example in Computer Science: the simplest possible program
+#   you can write (https://en.wikipedia.org/wiki/%22Hello,_World!%22_program)
+# - See the '#' sign at the beginning of the line? This is a *comment*: it
+#   means this line will not be considered a line of code, and will 'do'
+#   nothing. You can use them to add explanations!
+
+# This line, no '#', does something, telling Python to say: "Hello # World!"
+print("Hello World!")
